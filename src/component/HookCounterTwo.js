d6e72408f58df08d5de2fcbcba1dfaac13f69261
Gulam Anas <g.anas786@gmail.com>
2021-12-19 21:41:55 +0530

    counter using function parameter

--- src/component/HookCounterTwo.js
+++ src/component/HookCounterTwo.js
@@ -3,12 +3,20 @@ import React, { useState } from 'react'
 function HookCounterTwo() {
     const initialState = 0
     const [count, setCount] = useState(initialState)
+
+    const incrementFive = () => {
+        for (let i = 0; i < 5; i++) {
+            setCount(prevCount => prevCount + 1)
+        }
+    }
+
     return (
         <div>
             Count: {count}
             <button onClick={() => setCount(initialState)}>Reset</button>
-            <button onClick={() => setCount(count + 1)}>Increment</button>
-            <button onClick={() => setCount(count - 1)}>Decrement</button>
+            <button onClick={() => setCount(prevCount => prevCount + 1)}>Increment</button>
+            <button onClick={() => setCount(prevCount => prevCount - 1)}>Decrement</button>
+            <button onClick={incrementFive}>Increment 5</button>
         </div>
     )
 }

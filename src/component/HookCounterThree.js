b1df2dad4695f386b42d9bbfa6abb8638cacb219
Gulam Anas <g.anas786@gmail.com>
2021-12-19 21:56:43 +0530

    controlling input using spread operator

--- src/component/HookCounterThree.js
+++ src/component/HookCounterThree.js
@@ -5,10 +5,11 @@ function HookCounterThree() {
     const [name, setName] = useState({ firstName: '', lastName: '' })
     return (
         <div>
-            <input type="text" value={name.firstName} onChange={e => setName({ firstName: e.target.value })} />
-            <input type="text" value={name.lastName} onChange={e => setName({ lastName: e.target.value })} />
+            <input type="text" value={name.firstName} onChange={e => setName({ ...name, firstName: e.target.value })} />
+            <input type="text" value={name.lastName} onChange={e => setName({ ...name, lastName: e.target.value })} />
             <h1>First Name : {name.firstName} </h1>
             <h1>First Name : {name.lastName} </h1>
+            <h2>{JSON.stringify(name)}</h2>
         </div>
     )
 }
